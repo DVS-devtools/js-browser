3fecd097b36617dfe2926cbc6873d1bfd260760e
Lorenzo Mussi <lorenzo.mussi@docomodigital.com>
2018-10-15 17:35:43 +0200

    examples ended

--- example/index.js
+++ example/index.js
@@ -1,8 +1,29 @@
-import JsBrowser from '../src/index.js';
+import Browser from '../src/index.js';
 
-const foo = JsBrowser.init({
-    message: 'hello'
-});
+let pageIndex = 0;
+let paramIndex = 0;
+let params = '';
 
-document.write(foo);
-console.log(foo);
+window.shiftPage = () => {
+    Browser.shiftPage(params + pageIndex);
+    pageIndex += 1;
+
+
+    console.log(['Browser.getPrevPage:', Browser.getPrevPage()])
+    document.getElementById('prevPage').innerText = JSON.stringify(Browser.getPrevPage());
+};
+
+
+
+window.addQueryParams = () => {
+    params = params + `params${paramIndex}=${paramIndex}&`;
+    if (history.pushState) {
+        var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname +  `?${params}`;
+        window.history.pushState({ path: newurl }, '', newurl);
+    }
+
+    paramIndex += 1;
+
+    console.log(['Browser.getQueryParams:', Browser.getQueryParams()])
+    document.getElementById('queryParams').innerText = JSON.stringify(Browser.getQueryParams());
+}
