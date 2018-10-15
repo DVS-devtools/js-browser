8173c49674e135b20416e31520a54a1a715a3510
Lorenzo Mussi <lorenzo.mussi@docomodigital.com>
2018-10-15 15:56:44 +0200

    new test flow

--- src/getQueryParams/getQueryParams.test.js
+++ src/getQueryParams/getQueryParams.test.js
@@ -1,5 +1,20 @@
 import getQueryParams from './index';
 
-test('init() returns message + " world"', () => {
-    expect(true).toBe(true);
+describe('Get Query Params function', () => {
+    test('getQueryParams - Get no query params', () => {
+        const queryParams = getQueryParams();
+        expect(queryParams).toEqual({});
+    })
+    
+    test('getQueryParams - Get more query params after hashbang', () => {
+        window.history.pushState({}, 'Test Title', '#!/test.html?aqueryParams=1&anotherQueryParams=2');
+        const queryParams = getQueryParams();
+        expect(queryParams).toEqual({"aqueryParams": "1", "anotherQueryParams": "2"});
+    })
+    
+    test('getQueryParams - Get single query params after hashbang', () => {
+        window.history.pushState({}, 'Test Title', '#!/test.html?aqueryParams=1');
+        const queryParams = getQueryParams();
+        expect(queryParams).toEqual({"aqueryParams": "1"});
+    });
 });
--- src/index.test.js
+++ src/index.test.js
@@ -1,38 +1,37 @@
 import Browser from './';
 import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from 'constants';
 
-describe('Browser feature', () => {
+describe('Browser test', () => {
 
-    test('getPrevPage equal to null', () => {
+    test('Browser - getPrevPage equal to null', () => {
         expect(Browser.getPrevPage()).toBe(null);
     });
 
-    test('Get prevPage equal to null after shifting 1 page', () => {
+    test('Browser - Get prevPage equal to null after shifting 1 page', () => {
         Browser.shiftPage('/test');
         expect(Browser.getPrevPage()).toBe(null);
     });
 
-    test('Get prevPage equal to /test after shifting first to /test then to /anotherTest', () => {
+    test('Browser - Get prevPage equal to /test after shifting first to /test then to /anotherTest', () => {
         Browser.shiftPage('/test');
         Browser.shiftPage('/anotherTest');
         expect(Browser.getPrevPage()).toBe('/test');
     });
 
-    test('Get none query params', () => {
+    test('Browser - Get none query params', () => {
         const queryParams = Browser.getQueryParams();
         expect(queryParams).toEqual({});
-    })
+    });
 
-    test('Get more query params after hashbang', () => {
-        window.history.pushState({}, 'Test Title', '#!/test.html?aqueryParams=1&anotherQueryParams=2');
-        const queryParams = Browser.getQueryParams();
-        expect(queryParams).toEqual({"aqueryParams": "1", "anotherQueryParams": "2"});
-    })
-
-
-    test('Get single query params after hashbang', () => {
+    test('Browser - Get single query params after hashbang', () => {
         window.history.pushState({}, 'Test Title', '#!/test.html?aqueryParams=1');
         const queryParams = Browser.getQueryParams();
         expect(queryParams).toEqual({"aqueryParams": "1"});
     });
-})
+
+    test('Browser - Get more query params after hashbang', () => {
+        window.history.pushState({}, 'Test Title', '#!/test.html?aqueryParams=1&anotherQueryParams=2');
+        const queryParams = Browser.getQueryParams();
+        expect(queryParams).toEqual({"aqueryParams": "1", "anotherQueryParams": "2"});
+    });
+});
